a18d519fd94c00047f0c69880d4b26d96399d59f
andreypa09 <andrey.pavlovsky09@gmail.com>
2026-03-04 19:41:32 +0200

    04.03

--- main.py
+++ main.py
@@ -19,8 +19,34 @@ async def main():
     # https://catfact.ninja/fact
     @dp.message(Command(commands=['catfact']))
     async def get_cat_fact(message: Message):
-        fact = get('https://api.catfact.ninja/fact')
+        print(f'[LOG] Пользователь {message.from_user.id} вывел факт о кошках')
+        print('[LOG] Запрашиваю информацию с сервера')
+        response = get('https://catfact.ninja/fact')
+        print(f'[LOG] Получен результат со статусом {response.status_code}')
+        print(response.json()["length"])
+        await message.answer(response.json()["fact"])
+
+    @dp.message(Command(commands=['breeds']))
+    async def get_cat_breeds(message: Message):
+        print(f'[LOG] Пользователь {message.from_user.id} запрашивает породу кошки')
+        print('[LOG] Запрашиваю информацию с сервера')
+        response = get('https://catfact.ninja/breeds')
+        print(f'[LOG] Получен результат со статусом {response.status_code}')
+        response_json = response.json()
+        print(response_json['data'][0]['country'])
+        await message.answer(response_json["data"][0]['breed'])
+        print(f'[LOG] Пользователь {message.from_user.id} вывел породу кошки')
+
+    @dp.message()
+    async def text_handler(message: Message):
+        print(f"[LOG] Пользователь {message.from_user.id} ввел сообщение")
+        await message.answer('Не знаю такой команды')
+
+
+
     await dp.start_polling(bot)
-print(f'[LOG] Бот запущен')
+
+
+print(f'[LOG] Бот запущен.')
 if __name__ == '__main__':
     run(main()) # запускает цикла событий(dispatcher)
