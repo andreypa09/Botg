5f71564ab5bc99ebbdc5790e79b21cea4ca9c2de
andreypa09 <andrey.pavlovsky09@gmail.com>
2026-03-14 19:50:44 +0200

    14.03

--- main.py
+++ main.py
@@ -1,32 +1,70 @@
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
+from aiogram.filters import Command
 from asyncio import run
 from config import BOT_TOKEN
 import os
+import asyncio
+
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
-    @dp.message(F.photo | F.video)
+    @dp.message(F.photo | F.video | F.voice)
     async def get_photo_video(message: Message, bot: Bot):
         print(f"[LOG] Пользователь {message.from_user.id} вызвал функцию get_photo_video")
         os.makedirs("downloads", exist_ok=True)
-        if not message.photo:
-            file = await bot.get_file(message.video.file_id)
+        if message.photo:
+            file = await bot.get_file(message.photo[-1].file_id)
             print(f'[LOG] Файл {file.file_unique_id} получен')
             PATH = os.path.join("downloads", f"{file.file_unique_id}.jpg")
-
+        elif message.voice:
+            file = await bot.get_file(message.voice.file_id)
+            PATH = os.path.join("downloads", f"{file.file_unique_id}.ogg")
         else:
-            file = await bot.get_file(message.photo[-1].file_id)
+            file = await bot.get_file(message.video.file_id)
             print(f'[LOG] Файл {file.file_unique_id} получен')
             PATH = os.path.join("downloads", f"{file.file_unique_id}.mp4")
 
-
+        print(f'[LOG] Файл {file} сохраняется в {PATH}')
         await bot.download_file(file.file_path, destination=PATH)
-        print(f'[LOG] Файл {PATH} сохранен в соответствующую директорию')
+        print(f'[LOG] Файл {file} сохранен в {PATH}')
 
         await message.answer("крутое фото или видео")
 
+    @dp.message(F.sticker)
+    async def sticker_handler(message: Message):
+        print(f"[LOG] Пользователь {message.from_user.id} вызвал функцию sticker_handler")
+        with open("stickers.txt", "a+") as f:
+            f.write(message.sticker.file_id + '\n')
+            print(f"[LOG] ID стикера {message.sticker.file_id} сохранен в stickers.txt")
+
+
+    @dp.message(F.text == 'Отправь фото')
+    async def send_video(message: Message):
+        print(f'[LOG] Пользователь {message.from_user.id} вызвал функцию send_video')
+        # await message.answer_photo(
+        #     photo = "https://pin.it/IcbS7Pdyk",
+        #     caption = "Это кот"
+        # )
+        PATH = os.path.join("files", "20260123_152149.mp4")
+        print("[LOG] Начало бинаризации")
+        video = FSInputFile(PATH)
+        print("[LOG] Конец бинаризации")
+        await message.answer_video(
+            video = video,
+            caption = 'не за что'
+        )
+        print(f"Видео {PATH} отправлено с локального сервера")
+    @dp.message(Command(commands = ['show']))
+    async def show(message: Message):
+        if os.path.exists("show.txt"):
+            with open("show.txt", "a") as f:
+                list_data = f.readlines()
+                for i in list_data:
+                    elements = i.split(":")
+                    await message.answer(f'Текущая температура на улице: {elements[1]}')
+                    await asyncio.sleep(10)
 
     await dp.start_polling(bot)
 print(f'[LOG] Бот запущен.')
