9c3eb673a06041f559b51e6d1ee87022df88a1e5
andreypa09 <andrey.pavlovsky09@gmail.com>
2026-04-15 19:12:19 +0200

    //

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,7 +1,7 @@
 from aiogram import F, Router
-from aiogram.types import Message
-from lexicons.lexicon_ru import START_BTN_TEXT, COURSES_TEXT
-from keyboards.menu_kb import menu_kb
+from aiogram.types import Message, CallbackQuery
+from lexicons.lexicon_ru import START_BTN_TEXT, COURSES_TEXT, COURSES_INFO
+from keyboards.menu_kb import menu_kb, menu_join_kb
 router = Router()
 
 @router.message(F.text == START_BTN_TEXT[0])
@@ -9,4 +9,15 @@ async def courses_handler(message: Message):
     await message.answer(
         COURSES_TEXT,
         reply_markup=await menu_kb()
+    )
+
+
+@router.callback_query(F.data.startswith('course_'))
+async def menu_handler(data: CallbackQuery):
+    key = data.data
+    info_text = COURSES_INFO[key]
+    keyboard = await menu_join_kb()
+    await data.message.edit_text(
+        info_text,
+        reply_markup=keyboard
     )
--- keyboards/menu_kb.py
+++ keyboards/menu_kb.py
@@ -1,5 +1,5 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from lexicons.lexicon_ru import LIST_COURSES
+from lexicons.lexicon_ru import LIST_COURSES, JOIN_TEXT
 
 
 async def menu_kb():
@@ -12,5 +12,19 @@ async def menu_kb():
         list_btns.append([btn])
     keyboards = InlineKeyboardMarkup(
         inline_keyboard=list_btns
+    )
+
+    return keyboards
+
+async def menu_join_kb():
+    list_btns = []
+    for text, data in JOIN_TEXT.items():
+        btn = InlineKeyboardButton(
+            text=text,
+            callback_data=data
+        )
+        list_btns.append(btn)
+    keyboards = InlineKeyboardMarkup(
+        inline_keyboard=[list_btns]
     )
     return keyboards
