9d1cfb6df99f82ecd45d96d51e121a6a7d9d993e
andreypa09 <andrey.pavlovsky09@gmail.com>
2026-04-15 19:51:40 +0200

    //

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,7 +1,15 @@
 from aiogram import F, Router
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 from lexicons.lexicon_ru import START_BTN_TEXT, COURSES_TEXT, COURSES_INFO
 from keyboards.menu_kb import menu_kb, menu_join_kb
+from aiogram.fsm.state import State, StatesGroup
+
+class Registration(StatesGroup):
+    waiting_name = State()
+    waiting_username = State()
+
+
 router = Router()
 
 @router.message(F.text == START_BTN_TEXT[0])
@@ -20,4 +28,25 @@ async def menu_handler(data: CallbackQuery):
     await data.message.edit_text(
         info_text,
         reply_markup=keyboard
+    )
+
+
+@router.callback_query(F.data == "join_sign_up")
+async def sign_up_handler(data: CallbackQuery, state: FSMContext):
+    await data.message.answer("Введите ваше имя:")
+    await state.set_state(Registration.waiting_name)
+
+@router.message(Registration.waiting_name)
+async def process_handler(message: Message, state: FSMContext):
+    await message.answer("Введите юзернейм")
+    await state.update_data(name = message.text)
+    await state.set_state(Registration.waiting_username)
+
+
+@router.message(Registration.waiting_username)
+async def process_username(message: Message, state: FSMContext):
+    state_data = await state.get_data()
+    name = state_data.get("name", "undefined")
+    await message.answer(
+        f"Ваше имя: {name}\nВаш юзернейм: {message.text}"
     )
--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from asyncio import run
 from configs.config import BOT_TOKEN
 from handlers.start import router as start_router
 from handlers.menu import router as menu_router
+
+
+
+
+
 async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher()
 
     dp.include_router(start_router)
     dp.include_router(menu_router)
+
+
     await dp.start_polling(bot)
+
+
+
 print(f'[LOG] Бот запущен.')
 if __name__ == '__main__':
     run(main()) # запускает цикла событий(dispatcher)
